93f43f9e4f0b2236ce0e531a63fa3deebc703610
Mahadi Hassan Riyadh <mahadi_hassan3931@yahoo.com>
2021-08-18 22:34:19 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function updateCaseNumber(isIncreasing) {
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    if(isIncreasing == true){
+        caseInput.value = parseInt(caseNumber) + 1;
+    }
+    else if(isIncreasing == false){
+        caseInput.value = caseNumber - 1;
+    }
+    
+}
+
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateCaseNumber(true);
+})
+
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+})
